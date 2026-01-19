c4d8b7da9423186c64dadb7288a84ad94aa0b4a5
15daisuki <gugeyouxiang.teaone@gmail.com>
2026-01-19 23:10:10 +0900

    01.19-alian

--- 1.07_alien_invasion.py
+++ 1.07_alien_invasion.py
@@ -3,6 +3,7 @@ import pygame
 from settings import Settings
 from ship import Ship
 from bullet import Bullet
+from alien import Alien
 
 
 class AlienInvasion:
@@ -17,6 +18,9 @@ class AlienInvasion:
         self.clock = pygame.time.Clock()
         self.ship = Ship(self)
         self.bullets = pygame.sprite.Group()
+        self.aliens = pygame.sprite.Group()
+
+        self._create_fleet()
 
     def run_game(self):
         while True: # 游戏主循环
@@ -28,15 +32,15 @@ class AlienInvasion:
 
 
     def _check_events(self): # 处理事件（键盘 / 鼠标 / 退出）
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:  # 事件监听 用户请求关闭窗口的事件
-                    sys.exit()
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:  # 事件监听 用户请求关闭窗口的事件
+                sys.exit()
 
-                elif event.type == pygame.KEYDOWN: # 被按下的那一瞬间
-                    self._check_keydown_events(event)
+            elif event.type == pygame.KEYDOWN: # 被按下的那一瞬间
+                self._check_keydown_events(event)
 
-                elif event.type == pygame.KEYUP: # 被松开 的那一瞬间
-                    self._check_keyup_events(event)
+            elif event.type == pygame.KEYUP: # 被松开 的那一瞬间
+                self._check_keyup_events(event)
     def _check_keydown_events(self, event):
         # 响应按下
         if event.key == pygame.K_RIGHT:
@@ -50,9 +54,9 @@ class AlienInvasion:
 
     def _check_keyup_events(self, event):
         # 响应松开
-            if event.key == pygame.K_RIGHT:
+        if event.key == pygame.K_RIGHT:
                 self.ship.moving_right = False # 松开右键 → 飞机立刻停
-            elif event.key == pygame.K_LEFT:
+        elif event.key == pygame.K_LEFT:
                 self.ship.moving_left = False # 松开左键 → 飞机立刻停
 
     def _fire_bullet(self):
@@ -68,16 +72,38 @@ class AlienInvasion:
             if bullet.rect.bottom <= 0:
                 self.bullets.remove(bullet)
 
+    def _create_fleet(self):
+        alien = Alien(self)
+        alien_width, alien_height = alien.rect.size
+
+        current_x, current_y = alien_width, alien_height
+        while current_y < (self.settings.screen_height - 3 * alien_height):
+            while current_x < (self.settings.screen_width - 2 * alien_width):
+                self._create_alien(current_x, current_y)
+                current_x += 2* alien_width
+
+            current_x = alien_width
+            current_y += 2 * alien_height
+
+    def _create_alien(self, x_position, y_position):
+        new_alien = Alien(self)
+        new_alien.x = x_position
+        new_alien.rect.x = x_position
+        new_alien.rect.y = y_position
+        self.aliens.add(new_alien)
+
     def _update_screen(self):
-            self.screen.fill(self.settings.bg_color) # 背景色
-            for bullet in self.bullets.sprites():
-                bullet.draw_bullet()
-            self.ship.blitme()
-            pygame.display.flip() # 刷新屏幕
+        self.screen.fill(self.settings.bg_color) # 背景色
+        for bullet in self.bullets.sprites():
+            bullet.draw_bullet()
+        self.ship.blitme()
+        self.aliens.draw(self.screen)
+        pygame.display.flip() # 刷新屏幕
+
+
              # FPS
 
 if __name__ == '__main__':
     ai = AlienInvasion()
     ai.run_game()
-
 
